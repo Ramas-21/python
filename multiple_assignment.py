335d9f998178c5a33913a083690e556032113a51
Lawrence <lawrencehome21@gmail.com>
2023-09-29 16:20:33 -0700

    multiple assignment

--- multiple_assignment.py
+++ multiple_assignment.py
@@ -1,1 +1,11 @@
 # multiple assignment = allows us to assign multiple variables at the same time in one line of code
+name,age,height = "Lawrence",21,5.2
+print(name)
+# what if all the variables has the same value
+# eg lawrence = 30
+# kelvin = 30
+# dennis = 30
+# joseph = 30
+
+lawrence = kelvin = dennis = joseph = 30
+print(kelvin)
