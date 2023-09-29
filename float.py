6c6cfae5911258562784de604b5bef82f55527dd
Lawrence <lawrencehome21@gmail.com>
2023-09-29 09:27:55 -0700

    float data type

---
+++ float.py
@@ -0,0 +1,6 @@
+selling_price = 480.324
+buying_price = 182.021
+discount = selling_price - buying_price
+print("The discount is: "+str(discount))
+percentage_discount = discount / buying_price * 100
+print("The percentage discount is: "+str(percentage_discount)+"%")
