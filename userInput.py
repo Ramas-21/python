bf0e7527e7462dda791116fcd9bd966317c1ea87
Lawrence <lawrencehome21@gmail.com>
2023-10-02 15:31:44 -0700

    casted the height with a float datatype

--- userInput.py
+++ userInput.py
@@ -1,6 +1,6 @@
 name = input("What is your name? ")
 age = int(input("How old are you? ")) # int() or float() casting the users input (age)
-height = input("How tall are you? ")
+height = float(input("How tall are you? "))
 
 age = age + 1
 
