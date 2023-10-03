bf672d95ba65919fc4436a05a961fe8ee20db28e
Lawrence <lawrencehome21@gmail.com>
2023-10-03 15:10:55 -0700

    for loop

---
+++ forLoop.py
@@ -0,0 +1,13 @@
+# for loop = a statement that will executes its block of code a limited amount of times
+# while loop = unlimited
+# for Loop = limited
+
+for i in range(10):
+    print(i+1)
+
+for i in range(50,100+1,2):
+    print(i)
+
+for i in "Lawrence":
+    print(i)
+
